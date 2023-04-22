837d6183d9f95d40364e179987aa2a328827e1b3
rijulrr <rijulr@tamu.edu>
2023-04-21 21:16:55 -0500

    Added execution functionality

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,59 @@
-#![allow(unused)]
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
+use std::error::Error;
+use async_openai::{types::CreateCompletionRequestArgs, Client};
 use clap::Parser;
+use tokio::process::Command;
 
-// Search for a pattern in file and display lines that contain it
 #[derive(Parser)]
-struct Cli {
-    // pattern to look for
-    pattern: String,
-    // cross-platform path to file to read
-    path: std::path::PathBuf,
+#[command(author, version, about, long_about = None)]
+pub(crate) struct Cli {
+    /// The prompt given by the user
+    prompt: Vec<String>,
+
+    /// Execute generated text, such as shell commands
+    #[clap(short = 'y', long)]
+    force: bool,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+
+    let prompt: &str = args.prompt.first().map(String::as_str).unwrap_or_default();
+
+    let client: Client = Client::new();
+
+    let request = CreateCompletionRequestArgs::default()
+        .model("text-davinci-003")
+        .prompt(prompt)
+        .max_tokens(256_u16)
+        .temperature(0.0)
+        .top_p(1.0)
+        .best_of(1)
+        .logprobs(1)
+        .echo(true)
+        .frequency_penalty(0.0)
+        .presence_penalty(0.0)
+        .build()?;
+
+    let response = client.completions().create(request).await?;
+
+    let choice = response.choices.iter().nth(0).unwrap();
+
+    if args.force {
+        
+        let mut command = Command::new("bash");
+        command.arg("-c").arg(&choice.text);
+        let output = command.output().await?;
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+
+    } else {
+
+        println!("{}", choice.text);
+
+    } 
+
+    Ok(())
 }
-
-
